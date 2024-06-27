54866c438f2338ce57ba7861649dd27a508cc58d
Jafar Sherazi <jafarsherazi089@gmail.com>
2024-06-27 22:14:16 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 import streamlit as st
 from pytube import YouTube
 import os
+import base64
 
-def download_video(url, folder, download_type):
+def download_video(url, download_type):
     try:
         yt = YouTube(url)
         if download_type == "Video (MP4)":
             stream = yt.streams.filter(progressive=True, file_extension='mp4').first()
-            stream.download(folder)
         elif download_type == "Audio (MP3)":
             stream = yt.streams.filter(only_audio=True).first()
-            file_path = stream.download(output_path=folder)
-            base, ext = os.path.splitext(file_path)
+        
+        output_file = stream.download()
+        
+        if download_type == "Audio (MP3)":
+            base, ext = os.path.splitext(output_file)
             new_file = base + '.mp3'
-            os.rename(file_path, new_file)
-        return True
+            os.rename(output_file, new_file)
+            output_file = new_file
+        
+        return output_file
     except Exception as e:
         return str(e)
 
@@ -27,15 +32,19 @@ video_url = st.text_input("Paste YouTube URL here:")
 # Select download type
 download_type = st.radio("Select download type:", ("Video (MP4)", "Audio (MP3)"))
 
-# Browse and select download folder
-download_folder = st.text_input("Download folder path:", value=os.path.expanduser("~"))
-
 if st.button("Download"):
     if not video_url:
         st.error("Please paste a YouTube URL.")
     else:
-        result = download_video(video_url, download_folder, download_type)
-        if result is True:
-            st.success("Your video/audio downloaded successfully!")
+        file_path = download_video(video_url, download_type)
+        if not os.path.exists(file_path):
+            st.error(f"Error: {file_path}")
         else:
-            st.error(f"Error: {result}")
+            with open(file_path, "rb") as file:
+                btn = st.download_button(
+                    label="Click here to download",
+                    data=file,
+                    file_name=os.path.basename(file_path),
+                    mime="video/mp4" if download_type == "Video (MP4)" else "audio/mp3"
+                )
+            os.remove(file_path)
